b9ca53d665a223f08bbe77daa874d7409ad8d656
Picbel <picbel.gon@gmail.com>
2026-01-18 18:10:47 +0900

    구현중

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ repositories {
 
 dependencies {
     implementation("jakarta.mail:jakarta.mail-api:2.1.3")
-    implementation("org.eclipse.angus:angus-mail:2.0.3")
+    implementation("org.eclipse.angus:angus-mail:2.0.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     testImplementation(kotlin("test"))
 }
 
